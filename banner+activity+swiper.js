83b057714c3a6afdb1190754a48e8be569afaf45
ajeiafeiafj13r <faefjeijf22r>
2019-09-24 07:25:21 +0800

    整合區塊-(banner+activiity-swiper)-(by瑄)-2019-09-24

---
+++ banner+activity+swiper.js
@@ -0,0 +1,14 @@
+var swiper = new Swiper('.swiper-container', {
+    loop: true,
+    navigation: {
+        nextEl: '.button-next',
+        prevEl: '.button-prev',
+    },
+});
+var swiper = new Swiper('.swiper-container2', {
+    loop: true,
+    navigation: {
+        nextEl: '.button-next2',
+        prevEl: '.button-prev2',
+    },
+});
